fcc5f05ccb70ac1e62ef691081fb000710f045ed
Merzough Münker <mmuenker@digitaix.com>
2021-06-17 09:07:55 +0200

    build: add @rxap/schematics to setup script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ yarn nx run utilities:pack
 yarn --cwd dist/libs/utilities link
 
 yarn link @rxap/schematics-utilities
+
+yarn nx run schematics:pack
+
+yarn --cwd dist/libs/schematics link
+
+yarn link @rxap/schematics
