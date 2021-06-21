dc43f366cd3d469ea7d5d58dd34633925a87f861
Merzough Münker <mmuenker@digitaix.com>
2021-06-21 10:57:34 +0200

    feat: add function CheckIfPackagesAreInstalled

--- libs/utilities/src/index.ts
+++ libs/utilities/src/index.ts
@@ -8,3 +8,4 @@ export * from './lib/package-json-file';
 export * from './lib/package-json';
 export * from './lib/get-project';
 export * from './lib/file';
+export * from './lib/check-if-packages-are-installed'
