5280297f61938fca2183d2278ab0007066fe5be4
Merzough Münker <mmuenker@digitaix.com>
2021-06-21 11:25:32 +0200

    feat: add function GuessProjectRoot

--- libs/utilities/src/index.ts
+++ libs/utilities/src/index.ts
@@ -11,3 +11,4 @@ export * from './lib/file';
 export * from './lib/check-if-packages-are-installed'
 export * from './lib/guess-schematic-root';
 export * from './lib/workspace';
+export * from './lib/guess-project-root';
