fa98c1c04e9a260f5fa81241c849e2bef96ea3b5
Merzough Münker <mmuenker@digitaix.com>
2021-06-21 11:52:25 +0200

    fix: update logic with the new utilities functions

--- libs/schematics/src/schematics/add-schematic/index.ts
+++ libs/schematics/src/schematics/add-schematic/index.ts
@@ -1,18 +1,7 @@
-import {
-  apply,
-  applyTemplates,
-  chain,
-  mergeWith,
-  move,
-  Rule,
-  SchematicsException,
-  Tree,
-  url
-} from '@angular-devkit/schematics';
+import { apply, applyTemplates, chain, mergeWith, move, Rule, Tree, url } from '@angular-devkit/schematics';
 import { AddSchematic } from '../schema';
 import { strings } from '@angular-devkit/core';
-import { createDefaultPath } from '@schematics/angular/utility/workspace';
-import { join } from 'path';
+import { GuessProjectName, GuessSchematicRoot, UpdateCollectionJson } from '@rxap/schematics-utilities';
 
 const { dasherize } = strings;
 
@@ -20,41 +9,31 @@ export default function(options: AddSchematic): Rule {
 
   return async (host: Tree) => {
 
-    const projectRootPath = await createDefaultPath(host, options.project as string);
-
-    if (!options.path) {
-      options.path = join(projectRootPath.replace(/\/src\/lib$/, ''), 'schematics');
-    } else if (options.path[0] === '/') {
-      options.path = join(projectRootPath, options.path);
-    }
+    const projectName = GuessProjectName(host, options);
+    const schematicRoot = GuessSchematicRoot(host, projectName);
 
     return chain([
       mergeWith(apply(url('./files'), [
         applyTemplates({
           ...strings,
-          ...options
+          ...options,
+          schemaId: [ dasherize(projectName), dasherize(options.name) ].join('-'),
         }),
-        move(options.path)
+        move(schematicRoot)
       ])),
-      (tree) => {
+      UpdateCollectionJson(collection => {
 
-        const collectionJsonPath = join(options.path!, 'collection.json');
-
-        if (!tree.exists(collectionJsonPath)) {
-          throw new SchematicsException(`Could not find collection.json in '${options.path}'`);
+        if (!collection.schematics) {
+          collection.schematics = {};
         }
 
-        const collectionJson = JSON.parse(tree.read(collectionJsonPath)!.toString());
-
-        collectionJson.schematics[ dasherize(options.name) ] = {
+        collection.schematics[dasherize(options.name)] = {
           description: options.description,
-          factory:     `./${dasherize(options.name)}/index`,
-          schema:      `./${dasherize(options.name)}/schema.json`
+          factory: `./${dasherize(options.name)}/index`,
+          schema: `./${dasherize(options.name)}/schema.json`
         };
 
-        tree.overwrite(collectionJsonPath, JSON.stringify(collectionJson, null, 2));
-
-      }
+      }, { projectName }),
     ]);
 
   };
