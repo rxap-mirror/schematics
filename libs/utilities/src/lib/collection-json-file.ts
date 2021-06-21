2b37709b0ea523cb4d3085d884ae6dc777614b08
Merzough Münker <mmuenker@digitaix.com>
2021-06-21 14:09:13 +0200

    feat(collection-json-file): support migration.json

--- libs/utilities/src/lib/collection-json-file.ts
+++ libs/utilities/src/lib/collection-json-file.ts
@@ -9,27 +9,47 @@ import {
 } from '@rxap/schematics-utilities';
 import { CollectionJson } from './collection-json';
 
-export function GetProjectCollectionJsonFilePath(host: Tree, projectName: string): string {
+export enum CollectionJsonType {
+  SCHEMATICS = 'schematics',
+  MIGRATIONS = 'migrations'
+}
+
+export function GetProjectCollectionJsonFilePath(host: Tree, projectName: string, type: CollectionJsonType = CollectionJsonType.SCHEMATICS): string {
 
   const projectPackageJson = GetProjectPackageJson(host, projectName);
   const projectRoot = GetProjectRoot(host, projectName);
 
-  if (projectPackageJson.schematics) {
-    const collectionJsonPath = join(projectRoot, projectPackageJson.schematics);
+  let collectionPath: string | undefined = undefined;
+
+  switch (type) {
+    case CollectionJsonType.SCHEMATICS:
+      collectionPath = projectPackageJson.schematics;
+      break;
+    case CollectionJsonType.MIGRATIONS:
+      collectionPath = projectPackageJson['ng-update']?.migrations;
+      break;
+
+    default:
+      throw new SchematicsException(`The collection json type '${type}' is not supported`);
+
+  }
+
+  if (collectionPath) {
+    const collectionJsonPath = join(projectRoot, collectionPath);
     if (host.exists(collectionJsonPath)) {
       return collectionJsonPath;
     } else {
-      throw new SchematicsException(`The collection.json path for the project '${projectName}' does not exists`);
+      throw new SchematicsException(`The collection json path of type '${type}' for the project '${projectName}' does not exists`);
     }
   } else {
-    throw new SchematicsException(`The project '${projectName}' does not have a schematics property in the package.json`);
+    throw new SchematicsException(`The project '${projectName}' does not have a '${type}' property in the package.json`);
   }
 
 }
 
-export function GetProjectCollectionJson(host: Tree, projectName: string): CollectionJson {
+export function GetProjectCollectionJson(host: Tree, projectName: string, type: CollectionJsonType = CollectionJsonType.SCHEMATICS): CollectionJson {
 
-  const collectionJsonFilePath = GetProjectCollectionJsonFilePath(host, projectName);
+  const collectionJsonFilePath = GetProjectCollectionJsonFilePath(host, projectName, type);
 
   return GetJsonFile(host, collectionJsonFilePath);
 
@@ -37,6 +57,7 @@ export function GetProjectCollectionJson(host: Tree, projectName: string): Colle
 
 export interface UpdateCollectionJsonOptions extends UpdateJsonFileOptions {
   projectName: string;
+  type?: CollectionJsonType;
 }
 
 export function UpdateCollectionJson(
@@ -45,7 +66,7 @@ export function UpdateCollectionJson(
 ): Rule {
   return tree => {
 
-    const collectionJsonFilePath = GetProjectCollectionJsonFilePath(tree, options.projectName);
+    const collectionJsonFilePath = GetProjectCollectionJsonFilePath(tree, options.projectName, options.type);
 
     return UpdateJsonFile(updater, collectionJsonFilePath, options);
 
