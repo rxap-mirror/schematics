a88b8957db2338f60c3c433b0dc487183c5ae13c
Merzough Münker <mmuenker@digitaix.com>
2021-06-22 15:42:11 +0200

    chore: add git commit commands

--- preversion.sh
+++ preversion.sh
@@ -5,10 +5,17 @@ LATEST_TAG=$(git describe --abbrev=0 --tags)
 echo "Run target 'readme' for each affected project"
 nx affected --base="$LATEST_TAG" --head="HEAD" --target readme
 
+git add "*/README.md"
+git commit -m "docs: generate README.md"
+
 echo "Run target 'update-package-group' for each affected project"
 nx affected --base="$LATEST_TAG" --head="HEAD" --target update-package-group
+
+git add "*/package.json"
+git commit -m "chore: update ng-update package group"
 
 echo "Run target 'update-peer-dependencies' for each affected project"
 nx affected --base="$LATEST_TAG" --head="HEAD" --target update-peer-dependencies
 
-git add .
+git add "*/package.json"
+git commit -m "chore: update package.json peer dependencies"
