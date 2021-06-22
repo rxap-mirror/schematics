63ef2d0d2145cded4b265dc680f8a132c2a07e52
Merzough Münker <mmuenker@digitaix.com>
2021-06-22 16:58:40 +0200

    chore: add affected build step

--- version.sh
+++ version.sh
@@ -3,19 +3,22 @@
 LATEST_TAG=$(git describe --abbrev=0 --tags)
 
 echo "Run target 'readme' for each affected project"
-nx affected --base="$LATEST_TAG" --head="HEAD" --target readme
+nx affected --base="$LATEST_TAG" --head="HEAD" --target readme --parallel --maxParallel 8
 
 git add "*/README.md"
 git commit -m "docs: generate README.md"
 
 echo "Run target 'update-package-group' for each affected project"
-nx affected --base="$LATEST_TAG" --head="HEAD" --target update-package-group
+nx affected --base="$LATEST_TAG" --head="HEAD" --target update-package-group --parallel --maxParallel 8
 
 git add "*/package.json"
 git commit -m "chore: update ng-update package group"
 
 echo "Run target 'update-peer-dependencies' for each affected project"
-nx affected --base="$LATEST_TAG" --head="HEAD" --target update-peer-dependencies
+nx affected --base="$LATEST_TAG" --head="HEAD" --target update-peer-dependencies --parallel --maxParallel 8
 
 git add "*/package.json"
 git commit -m "chore: update package.json peer dependencies"
+
+echo "Run target 'pack' for each affected project"
+nx affected --base="$LATEST_TAG" --head="HEAD" --target pack
