218f3fed507562af95ab3044a4a0ec01a7092aa8
Merzough Münker <mmuenker@digitaix.com>
2021-07-15 12:12:42 +0200

    feat: support new protocol component input

--- libs/table/src/schematics/generate/elements/columns/link-column.element.ts
+++ libs/table/src/schematics/generate/elements/columns/link-column.element.ts
@@ -1,25 +1,43 @@
-import { ElementDef, ElementExtends } from '@rxap/xml-parser/decorators';
+import { ElementAttribute, ElementDef, ElementExtends } from '@rxap/xml-parser/decorators';
 import { SourceFile } from 'ts-morph';
 import { strings } from '@angular-devkit/core';
 import { AddNgModuleImport, ToValueContext } from '@rxap/schematics-ts-morph';
 import { ColumnElement } from './column.element';
+import { NodeFactory } from '@rxap/schematics-html';
 
 const { dasherize, classify, camelize, capitalize } = strings;
 
 @ElementExtends(ColumnElement)
 @ElementDef('link-column')
 export class LinkColumnElement extends ColumnElement {
+
+  @ElementAttribute()
+  public protocol?: string;
+
   public template(): string {
-    return `
-    <th mat-header-cell
-    *matHeaderCellDef
-    ${this.__parent.hasFeature('sort') ? 'mat-sort-header' : ''}>
-    <ng-container i18n>${capitalize(this.name)}</ng-container>
-    </th>
-    <td mat-cell
-    [rxap-link-cell]="element${this.valueAccessor}"
-    *matCellDef="let element"></td>
-    `;
+
+    const headerAttributes: string[] = [
+      'mat-header-cell',
+      '*matHeaderCellDef'
+    ];
+    const rowAttributes: string[] = [
+      'mat-cell',
+      `[rxap-link-cell]="element${this.valueAccessor}"`,
+      '*matCellDef="let element"'
+    ];
+
+    if (this.__parent.hasFeature('sort')) {
+      headerAttributes.push('mat-sort-header')
+    }
+    if (this.protocol) {
+      rowAttributes.push(`protocol="${this.protocol}"`);
+    }
+
+    return [
+      NodeFactory('th', ...headerAttributes)(`<ng-container i18n>${capitalize(this.name)}</ng-container>`),
+      NodeFactory('td', ...rowAttributes)(),
+    ].join('\n');
+
   }
 
   public handleComponentModule({
