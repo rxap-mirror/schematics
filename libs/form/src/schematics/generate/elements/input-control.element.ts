e1ff6de7236cb00434b8ff4462a6ec16bdb43877
Merzough Münker <mmuenker@digitaix.com>
2021-07-16 08:55:38 +0200

    fix: extend default validators

--- libs/form/src/schematics/generate/elements/input-control.element.ts
+++ libs/form/src/schematics/generate/elements/input-control.element.ts
@@ -1,8 +1,11 @@
 import { ControlElement, ControlTypeElement } from './control.element';
 import { ElementAttribute, ElementDef, ElementExtends } from '@rxap/xml-parser/decorators';
-import { coerceArray } from '@rxap/utilities';
 import { ElementFactory } from '@rxap/xml-parser';
 import { IsNumberElement } from './validators/is-number.element';
+import { IsEmailElement } from './validators/is-email.element';
+import { IsUrlElement } from './validators/is-url.element';
+import { IsStringElement } from './validators/is-string.element';
+import { IsPhoneNumberElement } from './validators/is-phone-number.element';
 
 @ElementExtends(ControlElement)
 @ElementDef('input-control')
@@ -15,18 +18,45 @@ export class InputControlElement extends ControlElement {
   public inputType?: string;
 
   public postParse() {
-    if (this.inputType === 'number' || this.inputType === 'integer') {
-      this.validators = coerceArray(this.validators);
-      this.validators.push(ElementFactory(IsNumberElement, {}));
+    switch (this.inputType) {
+
+      case 'tel':
+        if (!this.validators.some(v => v instanceof IsPhoneNumberElement)) {
+          this.validators.push(ElementFactory(IsPhoneNumberElement, {}));
+        }
+        break;
+
+      case 'email':
+        if (!this.validators.some(v => v instanceof IsEmailElement)) {
+          this.validators.push(ElementFactory(IsEmailElement, {}));
+        }
+        break;
+
+      case 'number':
+      case 'integer':
+        if (!this.validators.some(v => v instanceof IsNumberElement)) {
+          this.validators.push(ElementFactory(IsNumberElement, {}));
+        }
+        break;
+
+      case 'url':
+        if (!this.validators.some(v => v instanceof IsUrlElement)) {
+          this.validators.push(ElementFactory(IsUrlElement, {}));
+        }
+        break;
+
+      case 'password':
+      case 'text':
+        if (!this.validators.some(v => v instanceof IsStringElement)) {
+          this.validators.push(ElementFactory(IsStringElement, {}));
+        }
+        break;
+
     }
     if (!this.type) {
       switch (this.inputType) {
 
-        case 'string':
-        case 'text':
-          this.type = ElementFactory(ControlTypeElement, { name: 'string' });
-          break;
-
+        case 'checkbox':
         case 'boolean':
           this.type = ElementFactory(ControlTypeElement, { name: 'boolean' });
           break;
@@ -34,6 +64,10 @@ export class InputControlElement extends ControlElement {
         case 'integer':
         case 'number':
           this.type = ElementFactory(ControlTypeElement, { name: 'number' });
+          break;
+
+        default:
+          this.type = ElementFactory(ControlTypeElement, { name: 'string' });
           break;
 
       }
