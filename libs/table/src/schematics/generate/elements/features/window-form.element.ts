832baad5bce9fd82bf0d665807d06dfd659ebed3
Merzough Münker <mmuenker@digitaix.com>
2021-07-20 10:59:47 +0200

    fix: add type to submit method

--- libs/table/src/schematics/generate/elements/features/window-form.element.ts
+++ libs/table/src/schematics/generate/elements/features/window-form.element.ts
@@ -1,30 +1,28 @@
-import { ParsedElement, ElementFactory } from '@rxap/xml-parser';
+import { ElementFactory, ParsedElement } from '@rxap/xml-parser';
 import {
-  ElementDef,
-  ElementChildTextContent,
   ElementChild,
-  ElementRequired,
   ElementChildRawContent,
+  ElementChildTextContent,
+  ElementDef,
+  ElementRequired,
 } from '@rxap/xml-parser/decorators';
 import { SubmitHandleMethod } from '@rxap/schematics-form';
-import { CreateButtonElement } from './create-button.element';
-import { EditActionElement } from './action-buttons/edit-action.element';
-import {
-  ToValueContext,
-  AddComponentProvider,
-} from '@rxap/schematics-ts-morph';
+import type { CreateButtonElement } from './create-button.element';
+import type { EditActionElement } from './action-buttons/edit-action.element';
+import { AddComponentProvider, AddNgModuleProvider, ToValueContext, } from '@rxap/schematics-ts-morph';
 import { strings } from '@angular-devkit/core';
 import { join } from 'path';
-import { Rule, chain, externalSchematic } from '@angular-devkit/schematics';
+import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
 import { GenerateSchema } from '../../schema';
 import { Writers } from 'ts-morph';
-import { MethodElement, ModuleElement } from '@rxap/schematics-xml-parser';
+import { MethodElement, ModuleElement, TypeElement } from '@rxap/schematics-xml-parser';
+import type { ActionButtonElement } from './action-buttons/action-button.element';
 
 const { dasherize, classify, camelize } = strings;
 
 @ElementDef('window-form')
 export class WindowFormElement implements ParsedElement {
-  public __parent!: CreateButtonElement | EditActionElement;
+  public __parent!: CreateButtonElement | EditActionElement | ActionButtonElement;
 
   @ElementChildRawContent()
   public template?: string;
@@ -43,8 +41,9 @@ export class WindowFormElement implements ParsedElement {
   @ElementRequired()
   public title!: string;
 
-  public postParse() {
+  public get coerceName(): string {
     if (!this.name) {
+      // dependent on the parent component an auto suffix is added
       let suffix: string = '';
       if (this.__parent.__tag === 'create-button') {
         suffix = '-create';
@@ -52,25 +51,39 @@ export class WindowFormElement implements ParsedElement {
       if (this.__parent.__tag === 'edit-action') {
         suffix = '-edit';
       }
+      if (this.__parent.__tag === 'action') {
+        suffix = '-' + (this.__parent as any).type
+      }
       this.name = dasherize(this.__parent.__parent.id) + suffix;
     }
+    return this.name;
+  }
 
+  public get openFormWindowMethodName() {
+    return 'Open' + classify(this.coerceName) + 'FormWindowMethod';
+  }
+
+  public get openFormWindowMethodModuleSpecifier() {
+    return 'open-' + dasherize(this.coerceName) + '-form-window.method';
+  }
+
+  public postParse() {
     this.__parent.module = ElementFactory(ModuleElement, {
-      name: classify(this.name) + 'FormComponentModule',
+      name: classify(this.coerceName) + 'FormComponentModule',
       form:
         './' +
         join(
-          dasherize(this.name) + '-form',
-          dasherize(this.name) + '-form.component.module'
+          dasherize(this.coerceName) + '-form',
+          dasherize(this.coerceName) + '-form.component.module'
         ),
     });
     this.__parent.method = ElementFactory(MethodElement, {
-      name: 'Open' + classify(this.name) + 'FormWindowMethod',
+      name: this.openFormWindowMethodName,
       from:
         './' +
         join(
-          dasherize(this.name) + '-form',
-          'open-' + dasherize(this.name) + '-form-window.method'
+          dasherize(this.coerceName) + '-form',
+          this.openFormWindowMethodModuleSpecifier,
         ),
     });
     if (!this.template) {
@@ -79,6 +92,14 @@ export class WindowFormElement implements ParsedElement {
         'forms',
         dasherize(this.form ?? this.__parent.__parent.id) + '.xml'
       );
+    }
+    // if the submit method does not have a adapter the method
+    // should have the form parameters
+    if (!this.submit.adapter) {
+      this.submit.method.parameterType = ElementFactory(TypeElement, {
+        name: 'I' + classify(this.coerceName) + 'Form',
+        from: join('..', dasherize(this.coerceName) + '-form', dasherize(this.coerceName) + '.form')
+      });
     }
   }
 
@@ -98,6 +119,40 @@ export class WindowFormElement implements ParsedElement {
         skipTsFiles: options.skipTsFiles,
       }),
       (tree) => {
+
+        const formComponentModuleFilePath = join(
+          dasherize(this.name!) + '-form',
+          dasherize(this.name!) + '-form.component.module.ts'
+        );
+
+        if (!tree.exists(join(options.path!, formComponentModuleFilePath))) {
+          console.log(
+            'Table window form component module path: ' +
+            join(options.path!, formComponentModuleFilePath)
+          );
+          throw new Error('The table window form component module is not generated');
+        }
+
+        const sourceFile =
+          project.getSourceFile(formComponentModuleFilePath) ??
+          project.createSourceFile(
+            formComponentModuleFilePath,
+            tree.read(join(options.path!, formComponentModuleFilePath))!.toString()
+          );
+
+        AddNgModuleProvider(
+          sourceFile,
+          this.openFormWindowMethodName,
+          [
+            {
+              moduleSpecifier: './' + this.openFormWindowMethodModuleSpecifier,
+              namedImports: [ this.openFormWindowMethodName ]
+            }
+          ]
+        )
+
+      },
+      (tree) => {
         const formComponentFilePath = join(
           dasherize(this.name!) + '-form',
           dasherize(this.name!) + '-form.component.ts'
@@ -106,7 +161,7 @@ export class WindowFormElement implements ParsedElement {
         if (!tree.exists(join(options.path!, formComponentFilePath))) {
           console.log(
             'Table window form component path: ' +
-              join(options.path!, formComponentFilePath)
+            join(options.path!, formComponentFilePath)
           );
           throw new Error('The table window form component is not generated');
         }
