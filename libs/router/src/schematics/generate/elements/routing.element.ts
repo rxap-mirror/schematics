7de1a632d2f5ec18c98d433a02c99ed085837e2f
Merzough Münker <mmuenker@digitaix.com>
2021-09-08 13:14:27 +0200

    fix: skip routing module creation

--- libs/router/src/schematics/generate/elements/routing.element.ts
+++ libs/router/src/schematics/generate/elements/routing.element.ts
@@ -1,10 +1,10 @@
+import { chain, noop, Rule } from '@angular-devkit/schematics';
+import { ToValueContext } from '@rxap/schematics-ts-morph';
 import { ParsedElement } from '@rxap/xml-parser';
 import { ElementChildren, ElementDef } from '@rxap/xml-parser/decorators';
-import { RouteElement } from './route.element';
 import { ArrayLiteralExpression } from 'ts-morph';
-import { chain, Rule } from '@angular-devkit/schematics';
-import { ToValueContext } from '@rxap/schematics-ts-morph';
 import { RoutingSchema } from '../schema';
+import { RouteElement } from './route.element';
 
 @ElementDef('definition')
 export class RoutingElement implements ParsedElement<Rule> {
@@ -31,7 +31,7 @@ export class RoutingElement implements ParsedElement<Rule> {
     }
     return chain([
       chain(this.routes.map(route => route.toValue({ project, options, sourceFile: appRoutingSourceFile }))),
-      tree => tree.overwrite(options.routingModule!, appRoutingSourceFile.getFullText()),
+      options.feature ? noop() : tree => tree.overwrite(options.routingModule!, appRoutingSourceFile.getFullText()),
     ]);
   }
 
