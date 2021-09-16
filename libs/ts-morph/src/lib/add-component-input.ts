851e29d502836e4575c206bbfec9fb7b842ecbb0
Merzough Münker <mmuenker@digitaix.com>
2021-09-16 13:19:50 +0200

    fix: check if constructor exists

--- libs/ts-morph/src/lib/add-component-input.ts
+++ libs/ts-morph/src/lib/add-component-input.ts
@@ -19,65 +19,70 @@ export function AddComponentInput(
 
   const componentClass = GetComponentClass(sourceFile);
 
-  if (componentInputDefinition.setAccessor) {
-    if (!componentClass.getSetAccessor(componentInputDefinition.name)) {
-      componentClass.addSetAccessor({
-        name: componentInputDefinition.name,
-        scope: Scope.Public,
-        parameters: [
-          {
-            name: componentInputDefinition.name,
-            type: componentInputDefinition.type
-          }
-        ],
-        docs: componentInputDefinition.docs,
-        statements: [
-          `this._${componentInputDefinition.name} = ${componentInputDefinition.name};`
-        ],
-        decorators: [
-          {
-            name: 'Input',
-            arguments: componentInputDefinition.selector ? [ componentInputDefinition.selector ] : []
-          }
-        ]
-      });
-      if (!componentClass.getProperty('_' + componentInputDefinition.name)) {
-        componentClass.addProperty({
-          name: '_' + componentInputDefinition.name,
-          scope: Scope.Private,
-          type: componentInputDefinition.type,
-          initializer: componentInputDefinition.initializer,
-          hasQuestionToken: !componentInputDefinition.initializer && !componentInputDefinition.required,
-          hasExclamationToken: !componentInputDefinition.initializer && componentInputDefinition.required,
-        })
+  if (!componentClass.getConstructors().some(cotr => cotr.getParameters()
+    .some(param => !!param.getScope() && param.getName() === componentInputDefinition.name))) {
+
+    if (componentInputDefinition.setAccessor) {
+      if (!componentClass.getSetAccessor(componentInputDefinition.name)) {
+        componentClass.addSetAccessor({
+          name:       componentInputDefinition.name,
+          scope:      Scope.Public,
+          parameters: [
+            {
+              name: componentInputDefinition.name,
+              type: componentInputDefinition.type,
+            },
+          ],
+          docs:       componentInputDefinition.docs,
+          statements: [
+            `this._${componentInputDefinition.name} = ${componentInputDefinition.name};`,
+          ],
+          decorators: [
+            {
+              name:      'Input',
+              arguments: componentInputDefinition.selector ? [componentInputDefinition.selector] : [],
+            },
+          ],
+        });
+        if (!componentClass.getProperty('_' + componentInputDefinition.name)) {
+          componentClass.addProperty({
+            name:                '_' + componentInputDefinition.name,
+            scope:               Scope.Private,
+            type:                componentInputDefinition.type,
+            initializer:         componentInputDefinition.initializer,
+            hasQuestionToken:    !componentInputDefinition.initializer && !componentInputDefinition.required,
+            hasExclamationToken: !componentInputDefinition.initializer && componentInputDefinition.required,
+          });
+        }
+        sourceFile.addImportDeclaration({
+          namedImports:    ['Input'],
+          moduleSpecifier: '@angular/core',
+        });
       }
-      sourceFile.addImportDeclaration({
-        namedImports: [ 'Input' ],
-        moduleSpecifier: '@angular/core'
-      });
+    } else {
+      if (!componentClass.getProperty(componentInputDefinition.name)) {
+        componentClass.addProperty({
+          name:                componentInputDefinition.name,
+          scope:               Scope.Public,
+          type:                componentInputDefinition.type,
+          initializer:         componentInputDefinition.initializer,
+          hasQuestionToken:    !componentInputDefinition.initializer && !componentInputDefinition.required,
+          hasExclamationToken: !componentInputDefinition.initializer && componentInputDefinition.required,
+          docs:                componentInputDefinition.docs,
+          decorators:          [
+            {
+              name:      'Input',
+              arguments: componentInputDefinition.selector ? [componentInputDefinition.selector] : [],
+            },
+          ],
+        });
+        sourceFile.addImportDeclaration({
+          namedImports:    ['Input'],
+          moduleSpecifier: '@angular/core',
+        });
+      }
     }
-  } else {
-    if (!componentClass.getProperty(componentInputDefinition.name)) {
-      componentClass.addProperty({
-        name: componentInputDefinition.name,
-        scope: Scope.Public,
-        type: componentInputDefinition.type,
-        initializer: componentInputDefinition.initializer,
-        hasQuestionToken: !componentInputDefinition.initializer && !componentInputDefinition.required,
-        hasExclamationToken: !componentInputDefinition.initializer && componentInputDefinition.required,
-        docs: componentInputDefinition.docs,
-        decorators: [
-          {
-            name: 'Input',
-            arguments: componentInputDefinition.selector ? [ componentInputDefinition.selector ] : []
-          }
-        ]
-      });
-      sourceFile.addImportDeclaration({
-        namedImports: [ 'Input' ],
-        moduleSpecifier: '@angular/core'
-      });
-    }
+
   }
   sourceFile.addImportDeclarations(structures);
 
